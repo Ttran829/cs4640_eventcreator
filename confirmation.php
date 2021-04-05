bc04bcd5cd4cf6bdc6c9a1f1c6fb40cee1a8e67c
Kimberly Vo <kimberlyvo@Kimberlys-MacBook-Pro.local>
2021-04-05 11:41:16 -0400

    styled conformation page

--- confirmation.php
+++ confirmation.php
@@ -5,17 +5,20 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   <link href="https://emoji-css.afeld.me/emoji.css" rel="stylesheet">
+  <link rel="stylesheet" href="CSS/styles.css">
   <title>Event Confirmation</title>    
 </head>
 <body>
 <?php session_start(); ?>
      
-  <div class="container">
-    <div class = "card">
-    <h3 align = "center">Event Confirmation</h3>
-    </h3>
+  <div class="container mt-3">
+  
+    <div class = "card" >
+    <h3 align = "center" class = "card-header">Event Confirmation</h3>
+    
        
-    <div class= "m-3">
+    <div class= "m-3" align = "center" >
+    <div >
     Event Name <font color="green" style="font-style:italic"><?php echo $_SESSION['eventname'] ?></font>
     <br/>
     
@@ -29,10 +32,10 @@
     Game Chosen <font color="green" style="font-style:italic"><?php echo $_SESSION['lunch'] ?></font>
 
     <br/>
-    
+    <div>
     <div>
   </div>
-  <button type="button" class="btn btn-primary" id="btn-submit">Confirm</button>
+  <button type="button" class="btn btn-primary m-3" id="btn-submit">Confirm</button>
   <div>
   
 </body>
