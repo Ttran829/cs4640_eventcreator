f97ac63ed0ea56f216d4c812923c160f7e1d927f
Tien Tran <ttran829@gmail.com>
2021-04-08 16:16:14 -0400

    Added database functionality for create event

--- create_event.php
+++ create_event.php
@@ -48,7 +48,7 @@ if (!isset($_SESSION['session_user_id'])) {
          <div class = "row justify-content-md-center">
              <div class = "col">
                 <div class ="card center mt-2">
-                  <h3 style= "text-align:center;" class = "m-3"> Welcome <?php if (isset($_SESSION['user'])) echo $_SESSION['user'] ?>, let's create an event!</h3>
+                  <h3 style= "text-align:center;" class = "m-3"> Welcome <?php if (isset($_SESSION['name'])) echo $_SESSION['name'] ?>, let's create an event!</h3>
                     <div class = "form m-5">
                     <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">   
                           <div id = "name" class="form-group mb-3">
@@ -92,6 +92,7 @@ if (isset($_POST['eventname']))
   header('Location: game_select.php');
 }
 ?>
+
                           <div class ="float-right mt-5"> 
                             <div class = "row">
                            <!--- <a href = "event_dashboard.html"> Cancel </a> --->
